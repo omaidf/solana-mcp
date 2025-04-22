21cbf674006e95fc3040b8b18bde674f7b1d2173
Omaid Faizyar <omaid@faizyar.com>
2025-04-22 10:48:43 -0700

    upgrade

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Framework :: FastAPI",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
